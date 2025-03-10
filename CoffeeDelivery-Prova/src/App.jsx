da45dff7b55a8dfb0af0b3b96ffdd48f8de3ca29
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2025-03-10 15:32:23 -0300

    Alterações..

--- CoffeeDelivery-Prova/src/App.jsx
+++ CoffeeDelivery-Prova/src/App.jsx
@@ -2,7 +2,12 @@ import { CardTrad } from "./components/card";
 
 export function App() {
   return (
-    <CardTrad foto={"src/img/tradicional.png"} tipo={"tradicional"}
-      tipo2={"Expresso tradicional"} desc={"O tradicional café feito com água quente e grãos muidos"} preco={"9,90"} />
-  )
+    <CardTrad
+      foto={"src/img/tradicional.png"}
+      tipo={"TRADICIONAL"}
+      tipo2={"Expresso tradicional"}
+      desc={"O tradicional café feito com água quente e grãos muidos"}
+      preco={"9,90"}
+    />
+  );
 }
